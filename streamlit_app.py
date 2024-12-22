153b48de439c4555a416ed8cf46ab6512e3a27f7
Tnelso95 <150095780+Tnelso95@users.noreply.github.com>
2024-12-22 11:48:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+## Home page for Gustavus Baseball
+## Landing page that has access to all of the different pages and resources
+## 
+
+st.set_page_config(
+    page_title="Gustavus Baseball Hub",
+    page_icon="https://static.wixstatic.com/media/7383ad_9fe67936506547e19ad5985ef14c8142~mv2.png/v1/fill/w_400,h_400,al_c,q_85,usm_1.20_1.00_0.01,enc_avif,quality_auto/CBH%20Logo.png",
 )
+
+# Add custom CSS for background image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://img.freepik.com/free-photo/light-background_24972-1415.jpg");
+        background-size: cover; /* Adjust background size */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title('Gustavus Baseball Hub')
+
+
+
+
+
+
